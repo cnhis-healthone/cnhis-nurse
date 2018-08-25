1872fceec9d6abc5044aecd22bd0d67c969d5d2b
MrDavidWNM <849723224@qq.com>
2018-08-25 09:33:55 +0800

    Revert "Revert "左侧导航""
    This reverts commit aa22a864b2cb0a96cfd37437ddc803906a1c167b.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import BusinessPlatformIndex from '../views/business-platform/Index.vue';
+import contain from '../views/business-platform/text/contain.vue';
 import StatisticalAnalysisIndex from '../views/statistical-analysis/Index.vue';
 import ParameterSettingIndex from '../views/parameter-setting/Index.vue';
 import MessageCenterIndex from '../views/message-center/Index.vue';
@@ -16,7 +17,14 @@ let router = new Router({
     },
     {
       path: '/business-platform',
-      component: BusinessPlatformIndex
+      component: BusinessPlatformIndex,
+      children: [
+	      {
+	        path: '/business-platform/text',
+	        left: contain
+	       
+	      }
+      ]
     },
     {
       path: '/statistical-analysis',
---
+++ src/views/business-platform/text/contain.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="">
+		测试
+	</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
